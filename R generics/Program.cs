5cee1f9fd34178b68a75d302cc746599af1d6f05
daliamohamed2000 <dm1956@fayoum.edu.eg>
2025-07-26 04:54:01 +0300

    Add project files.

---
+++ R generics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace R_generics
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Range<int> r1 = new Range<int>(1, 10);
+            //Range<int> r2 = new Range<int>(17, 10);
+            Console.WriteLine(r1.IsInRange(3));
+            Console.WriteLine(r1.IsInRange(11));
+            Console.WriteLine(r1.Lenght() + "\n");
+
+            Range<char> r3 = new Range<char>('a', 'd');
+            Console.WriteLine(r3.IsInRange('c'));
+            Console.WriteLine(r3.IsInRange('g'));
+            Console.WriteLine(r3.Lenght() + "\n");
+
+            Range<double> r4 = new Range<double>(2.5, 6.5);
+            Console.WriteLine(r4.IsInRange(3.76));
+            Console.WriteLine(r4.IsInRange(9.88));
+            Console.WriteLine(r4.Lenght() + "\n");
+        }
+    }
+}
---
+++ R generics/Range.cs
@@ -0,0 +1,17 @@
+﻿namespace R_generics
+{
+    internal class Range<t> where t : IComparable<t>
+    {
+        public t min { get; }
+        public t max { get; }
+        public Range(t min, t max)
+        {
+            if (min.CompareTo(max) > 0) throw new ArgumentException("min can't be greater than max");
+
+            this.min = min;
+            this.max = max;
+        }
+        public bool IsInRange(t val) => val.CompareTo(min) >= 0 && val.CompareTo(max) <= 0;
+        public string Lenght() => $"length = {(dynamic)max - (dynamic)min}";
+    }
+}
